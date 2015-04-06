ff1a3f384e0964674b49204dd264d07ba853325b
Pereiro <s.bobretsov@gmail.com>
2015-04-06 10:30:24 +0300

    Add cmd parameters for config files

--- config.go
+++ config.go
@@ -47,13 +47,13 @@ func (cf *Conf) Load(filename string) error {
 	return nil
 }
 
-func (cf *Conf) Save() error {
+func (cf *Conf) Save(filename string) error {
 	data, err := json.Marshal(cf)
 	if err != nil {
 		log.Error("json marshall  error:%s\n,err")
 		return err
 	}
-	err = ioutil.WriteFile(CONFIG_FILE, data, 0644)
+	err = ioutil.WriteFile(filename, data, 0644)
 	if err != nil {
 		log.Error("file error")
 		return err
--- relay.go
+++ relay.go
@@ -13,14 +13,14 @@ var(
 func main() {
 
     var logConfigFile = flag.String("logconfig","/usr/local/relay/logconfig.xml","enter path to file with log settings. Default:/usr/local/relay/logconfig.xml")
-    var mainConfigFile = flag.String("config","/usr/local/relay/config.json","enter path to file with log settings. Default:/usr/local/relay/config.json")
+    var mainConfigFile = flag.String("config","/usr/local/relay/config.json","enter path to config file. Default:/usr/local/relay/config.json")
 
     flag.Parse()
 
-    InitLogger(logConfigFile)
+    InitLogger(*logConfigFile)
     log.Info("Starting..")
     conf=new(Conf)
-    if err:=conf.Load(mainConfigFile);err!=nil{
+    if err:=conf.Load(*mainConfigFile);err!=nil{
         log.Critical("can't load config,shut down:",err.Error())
         panic(err.Error())
     }
