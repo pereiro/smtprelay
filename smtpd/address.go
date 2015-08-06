8c4de0292d42eafa7adaeaa60bc4799047ab7619
pereiro <s.bobretsov@gmail.com>
2015-08-06 17:14:00 +0300

    fix smtpd parseAddres func

--- smtpd/address.go
+++ smtpd/address.go
@@ -6,6 +6,11 @@ import (
 )
 
 func parseAddress(src string) (string, error) {
+
+	if len(src)==0 {
+		return "", fmt.Errorf("Ill-formatted e-mail address: %s", src)
+	}
+
 	if src[0] != '<' || src[len(src)-1] != '>' {
 		return "", fmt.Errorf("Ill-formatted e-mail address: %s", src)
 	}
